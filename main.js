207a30181056c4946f48b272a8944b5030740db9
Laís <laismartelini03@gmail.com>
2023-12-23 17:50:46 -0300

    finalizado

---
+++ main.js
@@ -0,0 +1,5 @@
+let btn_header = document.querySelector('#btn-header');
+
+btn_header.addEventListener('click', ()=>{
+    window.location.href = 'pag1.html';
+});
---
+++ pag1.js
@@ -0,0 +1,5 @@
+let btn_pag1 = document.querySelector('#btn-pag1');
+
+btn_pag1.addEventListener('click', ()=>{
+    window.location.href = 'pag2.html';
+})
